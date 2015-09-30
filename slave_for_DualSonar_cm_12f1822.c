ad93524e6f2a0889a528dcbdc7d6e4b081d4237e
hinagesi_708 <hinagesi-708@users.noreply.github.com>
2015-09-30 19:05:09 +0900

    Update slave_for_DualSonar_cm_12f1822.c

--- slave_for_DualSonar_cm_12f1822.c
+++ slave_for_DualSonar_cm_12f1822.c
@@ -1,11 +1,9 @@
-
 /* 
- * File:   slave_for_DualSonar_cm_12f1822.c
+ * File:   slave_for_DualSonar_cm_rev_12f1822.c
  * Author: kayoko
  *
- * Created on 2015/08/05, 09:00
+ * Created on 2015/09/30, 17:37
  */
-
 /********************************************
  *  compiler    MPLAB XC8(v1.34)            *
  *  PIC         PIC12F1822                  *
@@ -13,12 +11,13 @@
  *                                          *
  *  use_port                                *
  *                __________                *
- *          Vdd---|1  ●  8|---Vss         *
+ *          Vdd---|1  ?  8|---Vss         *
  *    (RA5)cm_2---|2       7|---cm_1(RA0)   *
  *      (RA4)  ---|3       6|---SCL(RA1)    *
  *      (RA3)  ---|4       5|---SDA(RA2)    *
  *                ==========                *
  ********************************************/
+
 #include <xc.h>
 #include "I2C_slave.h"
 
@@ -28,120 +27,111 @@
 #pragma config CP       = OFF
 #pragma config BOREN    = ON
 #pragma config FCMEN    = OFF
-#pragma config MCLRE    = ON
+#pragma config MCLRE    = OFF
 #pragma config CPD      = OFF
 #pragma config IESO     = OFF
 #pragma config FOSC     = INTOSC
 
-#pragma config LVP      = ON
+#pragma config LVP      = OFF
 
 #define _XTAL_FREQ 16000000
 
-
 void init();
+unsigned long count_time = 0;
+
+static void interrupt ForInterrupt(){
+#include "I2C_slave_int.h"
+}
+
 
 int main(void) {
     init();
     I2C_init();
-    
-    int cm_1;
-    int cm_2;
 
     while (1) {
-        cm_1 = 0;
-        cm_2 = 0;
-        cm_1 =  Pls_cm_1();
-        cm_2 =  Pls_cm_2();
+        TRISA0 = 0;         //OUTPUT
+        RA0 = 0;            //LOW
+        __delay_us(2);
+        RA0 = 1;            //HIGH
+        __delay_us(5);
+        RA0 = 0;            //LOW
+    
+        TRISA0 = 1;
+        if(RA0 == 1){
+            TMR1L = 0;
+            TMR1H = 0;
+            TMR1IF = 0;
+            count_time = 0;
+            while(RA0 == 1){
+                if(TMR0IF){
+                    count_time += 65536;
+                    TMR1IF = 0;
+                }
+                if(count_time > 1000000) break;
+            }
+            if(TMR1IF){
+                count_time += TMR1 + 65536;
+            }else{
+                count_time += TMR1;
+            }
+            count_time = count_time / 6.169463087248322;
+            if(count_time > 4000) count_time = 0;
+            send_data[0] = count_time % 0x100;
+            send_data[1] = count_time / 0x100;
+            __delay_ms(1);
+        }
         
-        cm_1 = (cm_1 - 1.644394)/1.42615; 
-        cm_1 = (cm_1 - 0.2)/1.021886;
-        cm_1 = (cm_1 + 6.2)/0.999543;
-        cm_1 = (cm_1 - 0.6)/1.004514;
-        cm_1 = (cm_1 - 5.394668)/1.339007;
         
-        cm_2 = (cm_2 - 1.644394)/1.42615; 
-        cm_2 = (cm_2 - 0.2)/1.021886;
-        cm_2 = (cm_2 + 6.2)/0.999543;
-        cm_2 = (cm_2 -0.6)/1.004514;
-        cm_2 = (cm_2 - 5.394668)/1.339007;
-        
-        send_data[0] = cm_1 % 0x100;     //dat1 = (char)data;
-        send_data[1] = cm_1 / 0x100;     //dat2 = (char)data >> 8;
-        send_data[2] = cm_2 % 0x100;
-        send_data[3] = cm_2 / 0x100;
-//      data = dat2 * 0x100 + dat1; $BFI$_=P$7$N:](B
-        __delay_ms(50);
+        TRISA5 = 0;         //OUTPUT
+        RA5 = 0;            //LOW
+        __delay_us(2);
+        RA5 = 1;            //HIGH
+        __delay_us(5);
+        RA5 = 0;            //LOW
+    
+        TRISA5 = 1;
+        if(RA5 == 1){
+            TMR1L = 0;
+            TMR1H = 0;
+            TMR1IF = 0;
+            count_time = 0;
+            while(RA5 == 1){
+                if(TMR0IF){
+                    count_time += 65536;
+                    TMR1IF = 0;
+                }
+                if(count_time > 1000000) break;
+            }
+            if(TMR1IF){
+                count_time += TMR1 + 65536;
+            }else{
+                count_time += TMR1;
+            }
+            count_time = count_time / 6.169463087248322;
+            send_data[2] = count_time % 0x100;
+            send_data[3] = count_time / 0x100;
+            __delay_ms(1);
+        }
     }
+
     return (0);
 }
 
 void init() {
-    OSCCONbits.IRCF = 0b1111;       //Set oscillator 16MHz
+    OSCCONbits.IRCF = 0b1101;       //Set oscillator 16MHz
     ANSELA  = 0x00;                 //Set RA pins digital
-    TRISA0 = 1;
-    TRISA5 = 1;
+    TRISA = 0xFF;
+    OPTION_REGbits.nWPUEN = 0;
+    WPUA = 0xFF;
 
-    PORTA = 0x00;                   //Set PORTA Low
+    T1CONbits.T1CKPS = 0x00;
+    T1CONbits.T1OSCEN = 0;
+    T1CONbits.nT1SYNC = 0;
+    T1CONbits.TMR1CS = 0;
+    TMR1L = 0x00;
+    TMR1H = 0x00;
+
+    T1CONbits.TMR1ON = 1;
+
     return;
-}
-
-int Pls_cm_1() {
-    int leng_cm;
-    
-    TRISA0 = 0;
-    RA0 = 0;
-    __delay_us(2);
-    RA0 = 1;
-    __delay_us(5);
-    RA0 = 0;
-    
-    TRISA0 = 1;
-    leng_cm = PulseIn_cm_1();
-    __delay_ms(1);
-    
-    return leng_cm;
-}
-int PulseIn_cm_1(){
-    int time_cm = 0;
-    while(RA0 == 1);
-    while(RA0 == 0);
-    while(RA0 == 1){
-        __delay_us(1);
-        time_cm ++;
-//        if(time_cm > 500) break;
-    }
-    time_cm = time_cm;
-    return time_cm;
-}
-int Pls_cm_2() {
-    int leng_cm;
-    
-    TRISA5 = 0;
-    RA5 = 0;
-    __delay_us(2);
-    RA5 = 1;
-    __delay_us(5);
-    RA5 = 0;
-    
-    TRISA5 = 1;
-    leng_cm = PulseIn_cm_2();
-    __delay_ms(1);
-    
-    return leng_cm;
-}
-int PulseIn_cm_2(){
-    int time_cm = 0;
-    while(RA5 == 1);
-    while(RA5 == 0);
-    while(RA5 == 1){
-        __delay_us(1);
-        time_cm ++;
-//        if(time_cm > 500) break;
-    }
-    time_cm = time_cm;
-    return time_cm;
-}
-
-static void interrupt forinterrupt(){
-    #include "I2C_slave_int.h"
 }
