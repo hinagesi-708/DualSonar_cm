10e26a6578ed572ac6fe22873400b2ad9af1a0d6
hinagesi_708 <hinagesi-708@users.noreply.github.com>
2015-10-05 14:58:57 +0900

    Update slave_for_DualSonar_cm_12f1822.c
    5回の平均となります．メモリ不足よりreseaved_dataとsend_dataの配列はは共に[8]から[4]となりました．どちらかをなくして[0]と[8]のようにすることもできます．

--- slave_for_DualSonar_cm_12f1822.c
+++ slave_for_DualSonar_cm_12f1822.c
@@ -47,74 +47,93 @@ static void interrupt ForInterrupt(){
 int main(void) {
     init();
     I2C_init();
-
+    char i = 0;
+    unsigned short average_left[5] = {0};
+    unsigned short average_right[5] = {0};
+    unsigned short average_ans_left = 0;
+    unsigned short average_ans_right = 0;
     while (1) {
-        TRISA0 = 0;         //OUTPUT
-        RA0 = 0;            //LOW
-        __delay_us(2);
-        RA0 = 1;            //HIGH
-        __delay_us(5);
-        RA0 = 0;            //LOW
-    
-        TRISA0 = 1;
-        if(RA0 == 1){
-            TMR1L = 0;
-            TMR1H = 0;
-            TMR1IF = 0;
-            count_time = 0;
-            while(RA0 == 1){
-                if(TMR0IF){
-                    count_time += 65536;
-                    TMR1IF = 0;
+        for( i = 0; i < 5; i++ ){
+            TRISA0 = 0;         //OUTPUT
+            RA0 = 0;            //LOW
+            __delay_us(2);
+            RA0 = 1;            //HIGH
+            __delay_us(5);
+            RA0 = 0;            //LOW
+
+            TRISA0 = 1;
+            if(RA0 == 1){
+                TMR1L = 0;
+                TMR1H = 0;
+                TMR1IF = 0;
+                count_time = 0;
+                while(RA0 == 1){
+                    if(TMR0IF){
+                        count_time += 65536;
+                        TMR1IF = 0;
+                    }
+                    if(count_time > 30000) break;
                 }
-                if(count_time > 1000000) break;
-            }
-            if(TMR1IF){
-                count_time += TMR1 + 65536;
-            }else{
-                count_time += TMR1;
-            }
-            count_time = count_time / 6.169463087248322;
-            if(count_time > 4000) count_time = 4000;
-            if(count_time < 0) count_time = 0;
-            send_data[0] = count_time % 0x100;
-            send_data[1] = count_time / 0x100;
-            __delay_ms(1);
-        }
-        
-        
-        TRISA5 = 0;         //OUTPUT
-        RA5 = 0;            //LOW
-        __delay_us(2);
-        RA5 = 1;            //HIGH
-        __delay_us(5);
-        RA5 = 0;            //LOW
-    
-        TRISA5 = 1;
-        if(RA5 == 1){
-            TMR1L = 0;
-            TMR1H = 0;
-            TMR1IF = 0;
-            count_time = 0;
-            while(RA5 == 1){
-                if(TMR0IF){
-                    count_time += 65536;
-                    TMR1IF = 0;
+                if(TMR1IF){
+                    count_time += TMR1 + 65536;
+                }else{
+                    count_time += TMR1;
                 }
-                if(count_time > 1000000) break;
+                count_time = count_time / 6.169463087248322;
+                if(count_time > 4000) count_time = 4000;
+                if(count_time < 20) count_time = 0;
+//                send_data[0] = count_time % 0x100;
+//                send_data[1] = count_time / 0x100;
+                average_left[i] = count_time;
+                __delay_ms(1);
             }
-            if(TMR1IF){
-                count_time += TMR1 + 65536;
-            }else{
-                count_time += TMR1;
+
+
+            TRISA5 = 0;         //OUTPUT
+            RA5 = 0;            //LOW
+            __delay_us(2);
+            RA5 = 1;            //HIGH
+            __delay_us(5);
+            RA5 = 0;            //LOW
+
+            TRISA5 = 1;
+            if(RA5 == 1){
+                TMR1L = 0;
+                TMR1H = 0;
+                TMR1IF = 0;
+                count_time = 0;
+                while(RA5 == 1){
+                    if(TMR0IF){
+                        count_time += 65536;
+                        TMR1IF = 0;
+                    }
+                    if(count_time > 30000) break;
+                }
+                if(TMR1IF){
+                    count_time += TMR1 + 65536;
+                }else{
+                    count_time += TMR1;
+                }
+                count_time = count_time / 6.169463087248322;
+                if(count_time > 4000) count_time = 4000;
+                if(count_time < 0) count_time = 0;
+//                send_data[2] = count_time % 0x100;
+//                send_data[3] = count_time / 0x100;
+                average_right[i] = count_time;
+                __delay_ms(1);
             }
-            count_time = count_time / 6.169463087248322;
-            if(count_time > 4000) count_time = 4000;
-            if(count_time < 0) count_time = 0;
-            send_data[2] = count_time % 0x100;
-            send_data[3] = count_time / 0x100;
-            __delay_ms(1);
         }
+        for( i = 0; i < 5; i++){
+            average_ans_left  += average_left[i];
+            average_ans_right += average_right[i];
+        }
+        average_ans_left  = average_ans_left  / 5;
+        average_ans_right = average_ans_right / 5;
+        
+        send_data[0] = average_ans_left  % 0x100;
+        send_data[1] = average_ans_left  / 0x100;
+        send_data[2] = average_ans_right % 0x100;
+        send_data[3] = average_ans_right / 0x100;
     }
 
     return (0);
